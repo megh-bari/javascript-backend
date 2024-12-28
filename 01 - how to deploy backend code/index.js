0663e3f7ce4e41760a5898a216e1133168ef7fd1
Megh Bari <meghbari01@gmail.com>
2024-12-28 15:19:19 +0530

    deploying backend code into production

---
+++ 01 - how to deploy backend code/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("megh_bari");
+});
+
+app.get('/login', (req,res)=>{
+res.send('<h1>please login</h1>')
+})
+
+app.get('/meghbari', (req,res)=>{
+    res.send('<b>Megh Bari</b>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
